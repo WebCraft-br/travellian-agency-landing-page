7d7e5a8e3ff0409aaa4cd20b09f2f140fc869c44
viniciusg23 <viniciusgonzagaguilherme@gmail.com>
2024-09-06 20:22:31 -0300

    fixed images path

--- afterBuild.sh
+++ afterBuild.sh
@@ -1,7 +1,7 @@
 # LC_ALL=C find "out" -type f -exec sed -i '' 's/\/_next/\.\/_next/g' {} +
 
-LC_ALL=C find "out" -type f -exec sed -i 's/\/images/\.\/images/g' {} +
+LC_ALL=C find "out" -type f -exec sed -i '' 's/\/images/\.\/images/g' {} +
 
-LC_ALL=C find "out/_next/static/css" -type f -exec sed -i 's/\/images/\..\/..\/..\/images/g' {} +
+LC_ALL=C find "out/_next/static/css" -type f -exec sed -i '' 's/.\/images/\..\/..\/..\/images/g' {} +
 
 echo "Fixed"
