cf47357569ea7ed3cdc30128feaf4c57f7d86215
viniciusg23 <viniciusgonzagaguilherme@gmail.com>
2024-09-06 20:58:27 -0300

    fixed images path

--- afterBuild.sh
+++ afterBuild.sh
@@ -1,6 +1,8 @@
 # LC_ALL=C find "out" -type f -exec sed -i '' 's/\/_next/\.\/_next/g' {} +
 
+
 LC_ALL=C find "out" -type f -exec sed -i 's/\/images/https:\/\/webcraft-br.github.io\/travellian-agency-landing-page\/images/g' {} +
+
 
 # LC_ALL=C find "out/_next/static/css" -type f -exec sed -i 's/.\/images/\..\/..\/..\/images/g' {} +
 
