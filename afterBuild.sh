24365499199f22e91ba56de31bc2370c2a140cd7
viniciusg23 <viniciusgonzagaguilherme@gmail.com>
2024-09-06 19:36:46 -0300

    image path fix

--- afterBuild.sh
+++ afterBuild.sh
@@ -1,4 +1,4 @@
-LC_ALL=C find "out" -type f -exec sed -i '' 's/\/_next/\.\/_next/g' {} +
+# LC_ALL=C find "out" -type f -exec sed -i '' 's/\/_next/\.\/_next/g' {} +
 
 
 LC_ALL=C find "out" -type f -exec sed -i '' 's/\/images/\.\/images/g' {} +
